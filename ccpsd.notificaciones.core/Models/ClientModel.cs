61e189531ed8b0c0c85f929ee9117556d71a556a
Notificaciones <daltoncabrera@gmail.com>
2015-08-30 23:12:53 -0400

    Pasando proyecto de notificaciones a git

---
+++ ccpsd.notificaciones.core/Models/ClientModel.cs
@@ -0,0 +1,44 @@
+﻿using ccpsd.notificaciones.core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ccpsd.notificaciones.core
+{
+    public class ClientModel
+    {
+            
+        public int? Id { get; set; }
+
+        public string Secret { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Description { get; set; }
+
+        [Required]
+        public ApplicationTypes ApplicationType { get; set; }
+
+        [Required]
+        public bool Active { get; set; }
+
+        public int RefreshTokenLifeTime { get; set; }
+
+        [MaxLength(256)]
+        public string ImgName { get; set; }
+
+        public bool RefreshKey { get; set; }
+
+        [MaxLength(256)]
+        public string AllowedOrigin { get; set; }
+
+      
+    }
+}
---
+++ ccpsd.notificaciones.core/Models/UserModel.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ccpsd.notificaciones.core
+{
+    public class UserModel
+    {
+
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }    
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+       
+        public DateTime JoinDate { get; set; }
+
+        [Required]
+        public Boolean Active { get; set; }
+
+        public string ImgName { get; set; }
+
+
+    }
+
+   
+}
